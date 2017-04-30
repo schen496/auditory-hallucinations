fd665753669e6b6470a311ad505c24e287fcf934
Lamtharn Hantrakul <hanoi7@gmail.com>
2017-04-30 11:49:03 -0400

    finished hyperparameter search script code

--- training_pipeline/lr_w_search/controller.py
+++ training_pipeline/lr_w_search/controller.py
@@ -1,62 +1,103 @@
 '''
 Lamtharn (Hanoi) Hantrakul
 29/4/2017
+
+=====================
+HYPERPARAMETER SEARCH
+=====================
 This is a controller for doing hyperparameter search over models used in the project.
 
 '''
 
-from training_pipeline.exp_models.CNN_LSTM_models import *
+from training_pipeline.exp_models.CNN_LSTM_models import *  # Contains the models we are testing
 from training_pipeline.lr_w_search.model_utils import *  # Contains some useful functions for loading datasets
 
-
 USE_TITANX = True  # Set to True if using the Linux Machine with TitanX
-data_name='TopAngle100_dataX_dataY.h5'  # Set to name of h5py dataset
+data_name = 'TopAngle100_dataX_dataY.h5'  # Set to name of h5py dataset
 
-"""Read a sample from h5py dataset and return key dimensions
+"""Main function call"""
+def main():
 
-    Example returns:
-        frame_h = 100
-        frame_w = 100
-        channels = 3
-        audio_vector_dim = 18
-"""
-frame_h, frame_w, channels, audio_vector_dim = returnH5PYDatasetDims(USE_TITANX=USE_TITANX,
-                                                                     data_name=data_name)
+    num_trials = 1
+    final_accuracies = []
+    learning_rates = []
+    weight_scales = []
 
-# load full dataset as an HDF5 matrix object for use in Keras model
-dataX_train, dataY_train, dataX_test, dataY_test = load5hpyData(USE_TITANX=USE_TITANX,
-                                                                data_name=data_name)
+    for i in range(num_trials):
 
-# create defined model with given hyper parameters
-model = CNN_LSTM_model(image_dim=(frame_h,frame_w,channels),
-                       audio_vector_dim=audio_vector_dim,
-                       learning_rate=0.4e-6,
-                       weight_init=0.01)
+        # random search over logarithmic space of hyperparameters
+        lr = 10**np.random.uniform(-3.0,-6.0)
+        ws = 10**np.random.uniform(-2.0,-4.0)
+        learning_rates.append(lr)
+        weight_scales.append(ws)
 
-# load custom callbacks
-loss_history = LossHistory()
-acc_history = AccuracyHistory()
-callbacks_list = [loss_history, acc_history]
+        """Read a sample from h5py dataset and return key dimensions
 
-# train the model
-model.fit(dataX_test,
-          dataY_test,
-          shuffle='batch',
-          epochs=20,
-          batch_size=10000,  # 10000 is the maximum number of samples that fits on TITANX 12GB Memory
-          validation_data=(dataX_test, dataY_test),
-          verbose=1,
-          callbacks = callbacks_list)
+            Example returns:
+                frame_h = 100
+                frame_w = 100
+                channels = 3
+                audio_vector_dim = 18
+        """
+        frame_h, frame_w, channels, audio_vector_dim = returnH5PYDatasetDims(USE_TITANX=USE_TITANX,
+                                                                             data_name=data_name)
 
-# Graph training history
-for param_history in callbacks_list:
-    visualizeData(param_history=param_history)
+        """Load full dataset as an HDF5 matrix object for use in Keras model
 
+                Example returns and corresponding matrix shapes:
+                    dataX_train.shape = (26000,100,100,3)
+                    dataY_train.shape = (26000,18)
+                    dataX_train.shape = (4000,100,100,3)
+                    dataY_train.shape = (4000,18)
+        """
+        dataX_train, dataY_train, dataX_test, dataY_test = load5hpyData(USE_TITANX=USE_TITANX,
+                                                                        data_name=data_name)
 
+        # create defined model with given hyper parameters
+        model = CNN_LSTM_model(image_dim=(frame_h,frame_w,channels),
+                               audio_vector_dim=audio_vector_dim,
+                               learning_rate=lr,
+                               weight_init=ws)
 
+        # load custom callbacks
+        loss_history = LossHistory()
+        acc_history = AccuracyHistory()
+        callbacks_list = [loss_history, acc_history]
 
+        # train the model
+        model.fit(dataX_test,
+                  dataY_test,
+                  shuffle='batch',
+                  epochs=20,
+                  batch_size=10000,  # 10000 is the maximum number of samples that fits on TITANX 12GB Memory
+                  validation_data=(dataX_test, dataY_test),
+                  verbose=1,
+                  callbacks = callbacks_list)
 
+        # Graph training history
+        plotAndSaveData(loss_history, "Loss", learning_rate_hp=lr, weight_hp=ws, title="Loss History")
+        plotAndSaveData(acc_history, "Accuracy", learning_rate_hp=lr, weight_hp=ws, title="Acc History")
 
+        # Save final accuracy
+        final_accuracies.append(acc_history.test_acc[-1])
+
+        # Make a directory to store the predicted spectrums
+        folder_name = '{lr:%s}-{ws:%s}' % (str(lr), str(ws))
+        dir_path = makeDir(folder_name)  # dir_path = "../graphs/predicted_spectrums/{lr:0.000597}-{ws:0.000759}"
+        # Run the model on some unseen data and save the predicted spectrums in the directory defined previously
+        genAndSavePredSpectrum(model,
+                               dir_path,
+                               window_length = 300,
+                               USE_TITANX=USE_TITANX,
+                               data_name=data_name)
+
+    #Once all trials are complete, make a 3D plot that graphs x: learning rate, y: weight scale and z: final_accuracy
+    plotAndSaveSession(learning_rates,weight_scales,final_accuracies)
+
+    print("--- {EVERYTHING COMPLETE HOMIEEEEEEEEE} ---")
+
+if __name__ == '__main__':
+    main()
 
 
 
--- training_pipeline/lr_w_search/model_utils.py
+++ training_pipeline/lr_w_search/model_utils.py
@@ -2,7 +2,15 @@ from training_pipeline.exp_models.CNN_LSTM_models import *
 from keras.utils.io_utils import HDF5Matrix
 from keras.callbacks import Callback
 from keras.models import save_model
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import os, errno
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
+#####################
+# Model related utils
 
 def getModel(model_name):
     """Get the correct model for training
@@ -25,6 +33,8 @@ def getModel(model_name):
 
     return model
 
+####################
+# Data related Utils
 
 def load5hpyData(USE_TITANX=True, data_name='TopAngle100_dataX_dataY.h5'):
     """Load h5py data and return HDF5 object corresponding to X_train, Y_train, X_test, Y_test
@@ -68,7 +78,6 @@ def load5hpyData(USE_TITANX=True, data_name='TopAngle100_dataX_dataY.h5'):
 
     return dataX_train, dataY_train, dataX_test, dataY_test
 
-
 def returnH5PYDatasetDims(USE_TITANX=True, data_name='TopAngle100_dataX_dataY.h5'):
     """Load h5py data and return the dimensions of data in the dataet
 
@@ -102,6 +111,8 @@ def returnH5PYDatasetDims(USE_TITANX=True, data_name='TopAngle100_dataX_dataY.h5
 
     return frame_h, frame_w, channels, audio_vector_dim
 
+########################
+# Custom Keras Callbacks
 
 class saveModelOnEpochEnd(Callback):
     """Custom callback for Keras which saves model on epoch end"""
@@ -136,6 +147,150 @@ class AccuracyHistory(Callback):
         self.train_acc.append(logs.get('acc'))
         self.test_acc.append(logs.get('val_acc'))
 
-def visualizeData(param_history):
+##################################
+# Plotting and saving figure utils
 
-    return 0
+def plotAndSaveData(param_history,y_label,learning_rate_hp,weight_hp,title="Parameter History"):
+    """Saves a matplotlib plot that graphs parameter history
+
+            Args:
+                param_history (Callback object): Keras object that contains information on the parameter history
+                y_label (string): label for y axis e.g "Loss"
+                learning_rate_hp (float): learning rate hyperparameter e.g 4e-7
+                weight_hp (float): weight scale hyperparameter e.g 0.005
+                title (string): title for saved graph - default="Parameter History"
+
+            Returns:
+                None
+
+            """
+
+    param_history = (vars(param_history))
+
+    colors = ['r','b']
+
+    for idx, param in enumerate(param_history):
+        param_values = param_history[param]
+        epochs = np.arange(len(param_values))  # epochs is 1,2,3...[num items in param values]
+        plt.plot(epochs, param_values, colors[idx])
+
+    # generate legend
+    red_patch = mpatches.Patch(color='red', label='train')
+    blue_patch = mpatches.Patch(color='blue', label='test')
+    plt.legend(handles=[blue_patch, red_patch], prop={'size': 10})
+
+    # Plot the graph
+    plt.xlabel('epochs')
+    plt.ylabel(y_label)
+    title = '%s-{lr:%6f}-{ws:%6f}' % (title, learning_rate_hp, weight_hp)
+    plt.title(title)
+    # plt.show()
+    plt.draw()
+    file_name = '{lr:%8f}-{ws:%6f}.png' % (learning_rate_hp, weight_hp)
+    plt.savefig('../graphs/training_history/' + file_name)
+    plt.close()
+
+def makeDir(dir_name):
+    """Checks if a directory exists and creates one if it doesn't exist
+
+              Args:
+                  dir_name (string): folder name e.g "{lr:0.000597}-{ws:0.000759}"
+
+              Returns:
+                  dir_name (string): complete relative directory name e.g "../graphs/predicted_spectrums/{lr:0.000597}-{ws:0.000759}"
+    """
+
+    dir_name = '../graphs/predicted_spectrums/' + dir_name
+    #print (os.path.exists(dir_name))
+    if not os.path.exists(dir_name):
+        try:
+            os.makedirs(dir_name)
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+    return dir_name
+
+def genAndSavePredSpectrum(model,save_img_path, window_length = 300,USE_TITANX=True, data_name='TopAngle100_dataX_dataY.h5'):
+    """Generates and saves predicted spectrums when using the model on an unseen test set
+
+              Args:
+                  model (Keras model object): model created during training
+                  save_img_path (string): path where to save images e.g "../graphs/predicted_spectrums/{lr:0.000597}-{ws:0.000759}"
+                  window_length (int): length of predicted window. Default = 300
+                  USE_TITANX (boolean): set True if using the Linux Computer with TITANX
+                  data_name (string): name of the dataset e.g 'TopAngle100_dataX_dataY.h5'
+
+              Returns:
+                  None
+        """
+    # Define the external SSD where the dataset resides in
+    if USE_TITANX:
+        data_dir = '/home/zanoi/ZANOI/auditory_hallucinations_data/'
+    else:
+        data_dir = '/Volumes/SAMSUNG_SSD_256GB/ADV_CV/data/'
+    file_name = data_dir + data_name
+
+    # Open the h5py file
+    with h5py.File(file_name, 'r') as hf:
+        print("Reading data from file..")
+        dataX_test = hf['dataX_test'][:]
+        dataY_test = hf['dataY_test'][:]
+    print("dataX_test.shape:", dataX_test.shape)
+    print("dataY_test.shape:", dataY_test.shape)
+    print("np.max(dataY)", np.max(dataX_test))
+    print("np.min(dataY)", np.min(dataY_test))
+
+    (num_frames, frame_h, frame_w, channels) = dataX_test.shape
+    num_windows = math.floor(num_frames / window_length)
+
+    for i in tqdm(range(num_windows)):
+        pred_idx = i * window_length
+        end_idx = pred_idx + window_length
+
+        trainPredict = model.predict(dataX_test)
+        trainScore = math.sqrt(mean_squared_error(dataY_test[pred_idx:end_idx, :], trainPredict[pred_idx:end_idx, :]))
+        print('Train score: %.3f RMSE' % (trainScore))
+
+        ##### PLOT RESULTS
+        trainPlot = model.predict(dataX_test[pred_idx:end_idx, :])
+        print(trainPlot.shape)
+        plt.subplot(3, 1, 1)
+        plt.imshow(trainPlot.T, aspect='auto')
+        plt.title('Model prediction')
+        plt.ylabel('Note bins')
+        plt.xlabel('Time (frames)')
+        plt.subplot(3, 1, 2)
+        plt.title('Ground Truth')
+        plt.ylabel('Note bins')
+        plt.xlabel('Time (frames)')
+        plt.annotate('RMSE: %.3f' % (trainScore), xy=(5, 5), xytext=(5, 33))
+        plt.imshow(dataY_test[pred_idx:end_idx, :].T, aspect='auto')
+        plt.subplot(3, 1, 3)
+        plt.imshow(dataY_test[pred_idx:end_idx, :].T, aspect='auto')
+        plt.colorbar()
+        plt.tight_layout()
+        plt.draw()
+        plt.savefig(save_img_path + str(i) + '.png')  # ../graphs/predicted_spectrums/{lr:0.000597}-{ws:0.000759}/1.png
+        # plt.show()
+        plt.close()
+
+def plotAndSaveSession(learning_rates,weight_scales,final_accuracies):
+    learning_rates = np.array(learning_rates)
+    weight_scales = np.array(weight_scales)
+    final_accuracies = np.array(weight_scales)
+
+    print (final_accuracies)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    surf = ax.scatter(learning_rates, weight_scales, final_accuracies)
+
+    ax.set_xlabel('Learning Rates')
+    ax.set_ylabel('Weight init')
+    ax.set_zlabel('Final accuracy')
+
+    # Add a color bar which maps values to colors.
+    #fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
